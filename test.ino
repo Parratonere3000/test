29b627c17dd6bb9d65bcaf299a5ba82bbfb27993
Parratonere3000 <alexandre.parrat.valles@gmail.com>
2016-02-18 10:00:32 +0100

    Add blink code

--- test.ino
+++ test.ino
@@ -1,9 +1,18 @@
+/*
+ * Copyright 2016 Alexandre
+ * Public Domain
+ */
+ 
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
-
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
 }
