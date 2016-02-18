e90ec41042f2e58f1ed1e0c2b29f91dd2f70dd21
Parratonere3000 <alexandre.parrat.valles@gmail.com>
2016-02-18 10:06:21 +0100

    78=7

--- test.ino
+++ test.ino
@@ -6,13 +6,13 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+  pinMode(7, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(7, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(7, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);              // wait for a second
 }
